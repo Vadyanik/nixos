992bb6498a652c5e1dfaf7b026a097af02bb25f2
vadyanik <omletgamer100@gmail.com>
2026-03-17 11:13:27 +0200

    rebuild: 2026-03-17 11:08:53

--- configuration.nix
+++ configuration.nix
@@ -248,6 +248,26 @@ in
     jack.enable = true;
   };
 
+  programs.obs-studio = {
+    enable = true;
+
+    package = (
+      pkgs.obs-studio.override {
+        cudaSupport = true;
+      }
+    );
+
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-gstreamer
+      obs-vkcapture
+    ];
+  };
+
+  programs.obs-studio.enableVirtualCamera = true;
+
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
 

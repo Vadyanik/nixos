a87b4dd43d24d59f53951976e5f00989b8b568a3
vadyanik <omletgamer100@gmail.com>
2026-03-17 11:19:31 +0200

    rebuild: 2026-03-17 11:19:31

--- configuration.nix
+++ configuration.nix
@@ -268,6 +268,18 @@ in
 
   programs.obs-studio.enableVirtualCamera = true;
 
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+
+  boot.kernelModules = [ "v4l2loopback" ];
+
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Virtual Camera" exclusive_caps=1
+  '';
+
+  security.polkit.enable = true;
+
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
 
